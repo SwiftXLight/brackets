7f13fef8ac60424e9d0902c5dfa79963d117443c
SwiftXLight <72575014+SwiftXLight@users.noreply.github.com>
2021-03-12 16:10:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,56 +1,7 @@
-module.exports = function check(str, bracketsConfig) {
-    var stack = [];
-
-    var lenstr = str.length;
-    if (lenstr % 2 != 0) return false;
-
-    var lenConfig = bracketsConfig.length;
-    var checkeven = 1;
-    var cheven;
-    var chClose;
-    for (var i = 0; i < lenstr; i++) {
-        var ch = str.charAt(i);
-        var close = false;
-        var open = false;
-        var even = false;
-        for (let j = 0; j < lenConfig; j++) {
-            if (bracketsConfig[j][0] == bracketsConfig[j][1] && ch == bracketsConfig[j][0]) {
-                even = true;
-                checkeven = (-1) * checkeven;
-                cheven = ch;
-                console.log('checkeven = ', checkeven)
-                break;
-            }
-            if (ch == bracketsConfig[j][0]) {
-                open = true;
-                break;
-            }
-            if (ch == bracketsConfig[j][1]) {
-                close = true;
-                chClose = bracketsConfig[j][0];
-                break;
-            }
-        }
-        if (even == true) {
-            if (checkeven == -1) {
-                stack.push(ch);
-                console.log('push=', ch);
-            }
-            if (checkeven == 1 && cheven == stack[stack.length - 1]) {
-                console.log(' pop stack[stack.length-1]=', stack[stack.length - 1], ', ch= ', ch)
-                stack.pop();
-            }
-        } else {
-
-            if (open === true) {
-                stack.push(ch);
-                console.log('push=', ch);
-            }
-            if (close === true && chClose == stack[stack.length - 1]) {
-                console.log(' pop stack[stack.length-1]=', stack[stack.length - 1], ', ch= ', ch)
-                stack.pop();
-            }
-        }
-    }
-    return (stack.length === 0);
+module.exports = (s, b) => {
+    for (let i = 0; i < 30; i++)
+      b.map(b => b[0] + b[1]).forEach(
+        pair => (s = s.includes(pair) ? s.split(pair).join("") : s)
+      );
+    return s ? false : true;
 };
