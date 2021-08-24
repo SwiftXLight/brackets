db570e5f8c039f1ca9c3f21600ac3acbf4641857
Andriy Panchenko <72575014+SwiftXLight@users.noreply.github.com>
2021-08-24 19:37:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-module.exports = (s, b) => {
-    for (let i = 0; i < 30; i++)
-      b.map(b => b[0] + b[1]).forEach(
-        pair => (s = s.includes(pair) ? s.split(pair).join("") : s)
-      );
-    return s ? false : true;
+module.exports = (str, bracketsConfig) => {
+  for (let i = 0; i < 30; i++)
+    bracketsConfig.map(bracketsConfig => bracketsConfig[0] + bracketsConfig[1]).forEach(
+      pair => (str = str.includes(pair) ? str.split(pair).join('') : str)
+    );
+  return str ? false : true;
 };
